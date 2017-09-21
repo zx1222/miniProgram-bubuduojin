530d1dcc0acf4080aa54b034372193e39cad5544
zx1222 <1655638981@qq.com>
2017-09-21 17:32:18 +0800

    我的页 收支明细添加下拉刷新和上拉加载更多效果测试

--- pages/user/index.js
+++ pages/user/index.js
@@ -5,7 +5,7 @@ Page({
                 userInfo: {},
                 badgeCount:5,
                 scoreCount:10,
-                dedailsList:[
+                detailsList:[
                     {
                         name:'大转盘',
                         time:'2017.08.12 13:45',
@@ -40,43 +40,15 @@ Page({
                          score: '5',
                          state: 0
                          // 0是加1是减
-                     },
-                     {
-                        name:'大转盘',
-                        time:'2017.08.12 13:45',
-                        score:'5',
-                        state:0
-                        // 0是加1是减
-                    },
-                     {
-                        name: '大转盘',
-                        time: '2017.08.12 13:45',
-                        score: '5',
-                        state: 1
-                        // 0是加1是减
-                    },
-                     {
-                         name: '大转盘',
-                         time: '2017.08.12 13:45',
-                         score: '5',
-                         state: 1
-                         // 0是加1是减
-                     },
-                     {
-                         name: '大转盘',
-                         time: '2017.08.12 13:45',
-                         score: '5',
-                         state: 0
-                         // 0是加1是减
-                     },
-                     {
-                         name: '大转盘',
-                         time: '2017.08.12 13:45',
-                         score: '5',
-                         state: 0
-                         // 0是加1是减
-                     },
-                ]
+                     }
+                ],
+                // 正在加载的隐藏
+                hideLoadMore:true,
+                // 分页和每次加载显示的个数
+                PageNum: 1,   // 设置加载的第几次，默认是第一次  
+                callbackcount: 5,
+                // 全部加载标识
+                LoadingComplete: false
         },
   onLoad: function (options) {
           var that=this;
@@ -92,5 +64,47 @@ Page({
   },
   onShareAppMessage: function () {
   
+  },
+// 请求返回列表数据
+  getDetailsList:function(){
+    
+    //   wx.request({
+    //       url: '',
+    //       data:'',
+    //       success(res){
+    //       }
+    //   })
+
+  },
+  loadMore:function(){
+
+  },
+  //下拉刷新
+  onPullDownRefresh: function () {
+      wx.showNavigationBarLoading() //在标题栏中显示加载
+
+      //模拟加载
+      setTimeout(function () {
+          // complete
+          wx.hideNavigationBarLoading() //完成停止加载
+          wx.stopPullDownRefresh() //停止下拉刷新
+      }, 1500);
+  },
+//     //上拉加载更多   
+  onReachBottom: function () {
+      var that=this
+      this.setData({
+          hideLoadMore: false
+      })
+     setTimeout(function(){
+       
+         var arr = that.data.detailsList;
+         console.log(arr)
+         arr.push(arr[1])
+         that.setData({
+             detailsList: arr,
+             hideLoadMore: true
+         })
+     },1500) 
   }
 })
