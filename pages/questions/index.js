57308627b31c7840dd63724d2472d63e04fab4aa
zx1222 <1655638981@qq.com>
2017-12-05 16:17:59 +0800

    添加问题列表动态数据

--- pages/questions/index.js
+++ pages/questions/index.js
@@ -1,23 +1,19 @@
 // pages/discover/idnex.js
 Page({
     data: {
-        questions: [
-            {
-                title: 'aaaaaa',
-                content: 'aaa'
-            },
-            {
-                title: 'aaaaaa',
-                content: 'aaa'
-            },
-            {
-                title: 'aaaaaa',
-                content: 'aaa'
-            }
-        ]
+        questions: [ ]
     },
     onLoad: function (options) {
-
+        var that=this
+        wx.request({
+            url: 'http://192.168.0.189/net_sindcorp_anniutingwenzhen/web/question/question/index',
+            method:'GET',
+            success:function(res){
+                that.setData({
+                    questions: res.data
+                })
+            }
+        })
     },
 
     onReady: function () {
